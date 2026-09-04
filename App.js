ccc7ccd791f746dd55b36036b2d89b88fdd79156
Marcelo Korin <167823583+NiroKz@users.noreply.github.com>
2026-09-04 19:20:37 +0000

    atualizando App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
 
@@ -33,11 +34,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <View style={styles.container}>
-        {dados.map((item)=>
+      <FlatList
+      style={{width: '100%', flex:1}}
+      data={dados}
+      keyExtractor={(item)=>item.id.toString()}
+      renderItem={({item})=>(
+        <View style={styles.card}>
+          <Image source={{uri: item.image}} style={{width:50, height:50}} />
           <Text>{item.title}</Text>
-        )}
-      </View>
+          <Text>{item.price}</Text>
+
+        </View>
+  )}
+      ></FlatList>
       <StatusBar style="auto" />
     </View>
   );
@@ -50,4 +59,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card:{
+    flexDirection: 'column',
+    color: '#FFFF',
+    elevation: 8,
+    padding: 16,
+    width: '100%'
+  },
+  
 });
